ca9907a4c9666755ac06ad7eb44e0192f8ed352d
Artem Kotik <miaow2@yandex.ru>
2020-09-15 00:04:51 +0300

    Удален ключ из setting.py

--- school/school/settings.py
+++ school/school/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "&bq)knue^q11%qdqh7e$qb$8j(*34dt^rzqhb!6si)o-+dy03@"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
